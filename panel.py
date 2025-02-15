d58c45fbe4885038986f2b16d82fba00a8a38fa2
JohnJames <barthos@gmx.de>
2025-02-15 17:02:19 +0100

    Class Dot added three methods and updated the init method of Class Slider.

--- panel.py
+++ panel.py
@@ -23,7 +23,7 @@ class Dot(pygame.sprite.Sprite):
         elif self.rect.centery > maxy:
             self.rect.centery = maxy
 
-        print(self.rect.centery)
+        #print(self.rect.centery)
 
     def draw(self, root_surf: pygame.Surface) -> None:
         #self.image.fill((0, 0, 0, 0))
@@ -35,12 +35,13 @@ class Dot(pygame.sprite.Sprite):
         root_surf.blit(self.image, self.rect)
 
 
-class slider(pygame.sprite.Sprite):
-    def __init__(self, posx: int, posy: int):
+class Slider(pygame.sprite.Sprite):
+    def __init__(self, posx: int=0, posy: int=0, color: tuple[int, int, int]=(200, 200, 200)):
         super().__init__()
 
         self.posx = posx
         self.posy = posy
+        self.color = color
 
         self.offsetx = 10
         self.offsety = 20
@@ -51,6 +52,17 @@ class slider(pygame.sprite.Sprite):
         self.image = pygame.surface.Surface((self.width + self.offsetx * 2, 300), pygame.SRCALPHA)
         self.rect = self.image.get_rect(left=self.posx, top=self.posy)
         
+    def get_width(self) -> int:
+        return self.rect.width
+    
+
+    def get_height(self) -> int:
+        return self.rect.height
+    
+
+    def set_position(self, posx: int, posy: int) -> None:
+        self.rect.left = posx
+        self.rect.top = posy
 
 
     def update(self, posy: int=256) -> None:
@@ -59,8 +71,51 @@ class slider(pygame.sprite.Sprite):
 
     def draw(self, root_surf: pygame.Surface) -> None:
         self.image.fill((0, 0, 0, 0))
-        pygame.draw.rect(self.image, (200, 200, 200), (self.offsetx, self.offsety, self.width, self.height), border_radius=10)
-        pygame.draw.rect(self.image, (0, 0, 0, 0), (self.offsetx + 4, self.offsety + 4, 12, self.height-8), border_radius=10)
+        pygame.draw.rect(self.image, self.color, (self.offsetx, self.offsety, self.width, self.height), border_radius=10)
+        pygame.draw.rect(self.image, (0, 0, 0, 0), (self.offsetx + 4, self.offsety + 4, 12, self.height - 8), border_radius=10)
         self.dot.draw(self.image)
+
+        root_surf.blit(self.image, self.rect)
+
+
+class Panel(pygame.sprite.Sprite):
+    def __init__(self, posx: int=0, posy: int=0, color: tuple[int, int, int]=(50, 200, 50)):
+        super().__init__()
+
+        self.posx = posx
+        self.posy = posy
+        self.color = color
+
+        # slider initial position
+        sliderposx = 20
+        sliderposy = 0
+        offset = 5
+
+        self.slider_red = Slider()
+        self.slider_green = Slider()
+        self.slider_blue = Slider()
+        
+        self.slider_red.set_position(sliderposx, sliderposy)
+        self.slider_width = self.slider_red.get_width()
+        self.slider_green.set_position(sliderposx + self.slider_width + offset, sliderposy)
+        self.slider_blue.set_position(sliderposx + self.slider_width * 2 + offset * 2, sliderposy)
+
+        surf_width = sliderposx * 2 + self.slider_width * 3 + offset * 2
+        surf_height = self.slider_red.get_height()
+
+        self.image = pygame.surface.Surface((surf_width, surf_height), pygame.SRCALPHA)
+        self.rect = self.image.get_rect(left=posx, top=self.posy)
+
+    def update(self) -> None:
+        pass
+
+    def draw(self, root_surf: pygame.Surface) -> None:
+        self.image.fill((0, 0, 0, 0))
+
+        pygame.draw.rect(self.image, self.color, (0, 0, self.rect.width, self.rect.height), border_radius=10)
+        self.slider_red.draw(self.image)
+        self.slider_green.draw(self.image)
+        self.slider_blue.draw(self.image)
+
 
         root_surf.blit(self.image, self.rect)
